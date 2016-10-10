43313db0a8b4c9fb4c005190619cb31528165db7
mbtc <matthew.btc@gmail.com>
2016-10-10 11:41:18 -0600

    added soft troll

--- JavaScript Final In-Class Project.js
+++ JavaScript Final In-Class Project.js
@@ -1,4 +1,5 @@
 //character name
+var player =
 alert ("Welcome Player 1")
 prompt("Your player name...")
 
@@ -11,21 +12,31 @@ var hitpoint == 20
 var chance
 
 //weapon choise
-var weapon = 
-var axe = hitpoint= ++ 15, chance =
-var sword = hitpoint++ 10, chance =
+var weapon == 
+var axe = hitpoint ++ 15, chance = 65
+var sword = hitpoint ++ 10, chance = 75
 //knives hit twice
-var knives =hitpoint++ 7, chance 
+var knives =hitpoint++ 7, chance = 55
 var staff =hitpoint++ 25, chance =
-var sourJems = hitpoint++ 5, chance = 75 
+var sourJems = hitpoint++ 5, chance = 95 
 
 alert("What is your ideal weapon?")
 prompt("An axe, sword, knives, staff, sourjems")
 
 //round 1: soft troll.
 //attack low,and uncommon, hp med, and 
+var softTroll
+var health = 50
+var hitpoint = 10
 
 alert ("You have left the village with your " + weapon + " in hand.")
 alert ("As you progress further from the village you come to a Soft Troll.")
+alert ("The Soft Troll seems unsettled by your " + weapon + ".")
+alert ("Prepare to fight the Soft Troll!")
 
-prompt ("The Soft Troll seems ")
+//run a chance gambler here
+//have chance run to hit 
+
+alert(player + " has delt " hitpoint )
+
+//have the Soft Troll attack once
