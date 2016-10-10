812aa4c9a3951d04663301122f761cd1da2b73c5
mbtc <matthew.btc@gmail.com>
2016-10-10 11:29:26 -0600

    updated info

---
+++ JavaScript Final In-Class Project.js
@@ -0,0 +1,30 @@
+//character name
+alert ("Welcome Player 1")
+prompt("Your player name...")
+
+//character proporties
+//before gameover
+var health
+//your damnage
+var hitpoint
+//your chance of hitting
+var chance
+
+//weapon choise
+var weapon = 
+var axe (hitpoint++ 15)
+var sword (hitpoint++ 10)
+var knives(hitpoint++ )
+var staff (hitpoint++ 25)
+var sourJems (hitpoint++ 0, )
+
+alert("What is your ideal weapon?")
+prompt("An axe, sword, knives, staff, sourjems")
+
+//round 1: soft troll.
+//attack low,and uncommon, hp med, and 
+
+alert ("You have left the village with your " + weapon + " in hand.")
+alert ("As you progress further from the village you come to a Soft Troll.")
+
+prompt ("The Soft Troll seems ")
