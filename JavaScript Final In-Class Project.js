86c298d61f117fd9cc58dc949ad734a0cfb2e5c1
mbtc <matthew.btc@gmail.com>
2016-10-12 11:52:50 -0600

    Add files via upload
    in class finished

--- JavaScript Final In-Class Project.js
+++ JavaScript Final In-Class Project.js
@@ -1,58 +1,63 @@
-//character name
-var player =
-alert ("Welcome Player 1")
-prompt("Your player name...")
+var randomPercent = (Math.random * 100)
+var enemyHealth = 0
+var areaLevel = 1
+var playerName = prompt("Welcome, what is your name adventurer?")
 
-//character proporties
-//before gameover
-var health == 100
-//your damnage
-var hitpoint == 20
-//your chance of hitting
-var chance
+alert(playerName + " ... what a swell name. But it will take more then a swell name to prove your worth!")
 
-//weapon choise
-var weapon == 
-var axe = hitpoint ++ 15, chance = 80
-var sword = hitpoint ++ 10, chance = 85
-//knives hit twice
-var knives =hitpoint++ 7, chance = 55
-var staff =hitpoint++ 25, chance = 65
-var sourJems = hitpoint++ 5, chance = 95 
+switch (option) {
+    case 1:
+        prompt ("Do you wish to fight and purge the evil in this world?")
+       if ("Yes", "yes") {
+           areaLevel = 1
+       } else {
+           alert("Then what are you doing here?")
+       }
+        break;
 
-alert("What is your ideal weapon?")
-prompt("An axe, sword, knives, staff, sourjems")
+    default:
+        break;
+}
 
-//round 2: soft troll.
-//***loop round over and over till troll or player has fallen
-//attack low,and uncommon, hp med, and 
-var softTroll
-var health = 50
-var hitpoint = 10
+switch (areaLevel) {
+    case 1:
+        alert("You travel for some time and have reached the open fields.")
+        alert("You stumble across a Soft Troll in the open feild.")
+        alert(playerName + " has stirred the Soft Troll!")
+        if (enemyHealth == 0) {
+            alert("Nice work, you have felled the Soft Troll!")
+            areaLevel = 2
+        }
+    case 2:
+        alert("You have now reached the thickened woods.")
+        alert("As you venture deeper into the woods it becomes compact. There are only two ways left...")
+        prompt("Left or right?")
+        if (randomPercent <= 50) {
+            alert("You should not have gone this way! There is a Jabberwalk blocking the path!")
+            alert("The Jabberwalk engages you!")
+            if (enemyHealth == 0) {
+                alert("Hmmm... maybe you are the hero of legend...")
+                areaLevel = 3
+            }
+        } else {
+            alert("Oh great, a Stupid, Loney Spider!")
+            alert(playerName + " engaged the Stupid, Lonely Spider")
+            if (enemyHealth == 0) {
+                alert("Spider: OUCH! Why wont anyone talk to me?")
+                alert("The Spider limps away.")
+                areaLevel = 3
+            }
+        }
+    case 3:
+        alert("Deeper into the woods you find a ruined castle.")
+        alert("At last are you ready to prove your worth?")
+        alert("DKW: I have guieded you here to rid of any who dare step into my rein!")
+        alert("DKW: I shall wipe you and leave no trace of you in this world!")
+        alert("The Dark King Warlock begins the battle!")
+        if (enemyHealth == 0) {
+            alert("How... how have you beaten true evil...")
+            alert("You have destroyed the major threat, the area is clear!")
+        }
 
-alert ("You have left the village with your " + weapon + " in hand.")
-alert ("As you progress further from the village you come to a Soft Troll.")
-alert ("The Soft Troll seems unsettled by your " + weapon + ".")
-alert ("Prepare to fight the Soft Troll!")
-
-//run a chance gambler here
-//have chance run to hit 
-
-alert(player + " has delt " + hitpoint " damage." )
-
-//have the Soft Troll attack once
-
-
-alert("The Soft troll has delt 10 damage")
-
-//level 2
-var tinyFairy
-var Jabberwalky
-
-alert("You have progressed deaper within the woods and find a split in the road.")
-prompt ("Left or right?")
-if (prompt=left) {
-    Tiny Fairy
-} else {
-    Jabberwalky
+        break;
 }
