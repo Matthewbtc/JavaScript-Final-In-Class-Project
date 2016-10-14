06e07680691d3a7c8778dbac7ad2627f156c31c7
mbtc <matthew.btc@gmail.com>
2016-10-14 11:51:22 -0600

    Add files via upload
    final commit

--- JavaScript Final In-Class Project.js
+++ JavaScript Final In-Class Project.js
@@ -1,31 +1,52 @@
 var randomPercent = (Math.random * 100)
-var enemyHealth = 0
-var areaLevel = 1
+function player(params) {
+    var playerName;
+    var weapon;
+    var weapon = 50
+    var health = 100
+}
+
+function enemy(params) {
+    var health = 50
+    var attack = 25
+}
+
 var playerName = prompt("Welcome, what is your name adventurer?")
 
 alert(playerName + " ... what a swell name. But it will take more then a swell name to prove your worth!")
+alert("What type of weapon do you prefer?")
+var weapon = prompt("Axe, sword, staff, sour jems.")
+console.log(player)
 
-switch (option) {
-    case 1:
-        prompt ("Do you wish to fight and purge the evil in this world?")
-       if ("Yes", "yes") {
-           areaLevel = 1
-       } else {
-           alert("Then what are you doing here?")
-       }
-        break;
+//switch
+var gameLevel = 1;
+var passWord = "yes";
 
-    default:
-        break;
+var passWordCheck;
+
+function CheckPassword() {
+    switch (gameLevel) {
+        case 1:
+            passWordCheck = prompt("Are you here to destroy the evil in this world?")
+            if (passWord == passWordCheck) {
+                gameLevel = 2;
+            }
+            CheckPassword();
+            break;
+    }
 }
 
+CheckPassword();
+//end question switch
+var areaLevel = 1
 switch (areaLevel) {
     case 1:
         alert("You travel for some time and have reached the open fields.")
         alert("You stumble across a Soft Troll in the open feild.")
         alert(playerName + " has stirred the Soft Troll!")
-        if (enemyHealth == 0) {
-            alert("Nice work, you have felled the Soft Troll!")
+        alert(playerName + " engaged the Soft Troll with the " + weapon + "!")
+        if (player.weapon >= enemy.health) {
+            alert("Nice work, you have felled the Soft Troll with your" + weapon + "!")
             areaLevel = 2
         }
     case 2:
@@ -35,27 +56,26 @@ switch (areaLevel) {
         if (randomPercent <= 50) {
             alert("You should not have gone this way! There is a Jabberwalk blocking the path!")
             alert("The Jabberwalk engages you!")
-            if (enemyHealth == 0) {
+            if (player.weapon >= enemy.health) {
                 alert("Hmmm... maybe you are the hero of legend...")
                 areaLevel = 3
             }
         } else {
             alert("Oh great, a Stupid, Loney Spider!")
             alert(playerName + " engaged the Stupid, Lonely Spider")
-            if (enemyHealth == 0) {
+            if (player.weapon >= enemy.health) {
                 alert("Spider: OUCH! Why wont anyone talk to me?")
                 alert("The Spider limps away.")
-                areaLevel = 3
+                areaLevel == 3
             }
         }
     case 3:
         alert("Deeper into the woods you find a ruined castle.")
-        alert("At last are you ready to prove your worth?")
-        alert("DKW: I have guieded you here to rid of any who dare step into my rein!")
-        alert("DKW: I shall wipe you and leave no trace of you in this world!")
+        alert("???: At last you have reached me. I have guieded you here to rid of any who dare step into my rein!")
+        alert("???: I shall wipe you and leave no trace of you in this world!")
         alert("The Dark King Warlock begins the battle!")
-        if (enemyHealth == 0) {
-            alert("How... how have you beaten true evil...")
+        if (player.weapon >= enemy.health) {
+            alert("The Dark King Warlock: How... how have you beaten true evil...")
             alert("You have destroyed the major threat, the area is clear!")
         }
 
